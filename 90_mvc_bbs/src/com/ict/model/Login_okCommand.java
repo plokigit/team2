cdb95ba1b9ad74a12327dac536c1efa506719061
Horim <osetiopica@gmail.com>
2021-06-18 09:42:26 +0900

    06/18-09:42

---
+++ 90_mvc_bbs/src/com/ict/model/Login_okCommand.java
@@ -0,0 +1,36 @@
+package com.ict.model;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ict.db.BVO;
+import com.ict.db.DAO;
+
+public class Login_okCommand implements Command {
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response) {
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pw");
+		BVO vo = new BVO();
+		vo.setWriter(id);
+		vo.setPwd(pwd);
+		List<BVO> bvo = DAO.getLogIn(vo);
+		
+		request.setAttribute("bvo", bvo);
+		request.getSession().setAttribute("bvo", bvo);
+		
+		for (BVO bvo2 : bvo) {
+			if(bvo!=null) {
+				request.getSession().setAttribute("id", bvo2.getWriter());
+				request.getSession().setAttribute("pw", bvo2.getPwd());
+				return "MyController?cmd=list&cPage=1";
+			}
+		}
+		
+		int fail=1;
+		request.setAttribute("fail", fail);
+		return "MyController?cmd=login";
+	}
+}
